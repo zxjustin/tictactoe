4eec0ec21082dc21c7a1e72b9c31a4b643abeb44
zxJustin <zxjustin@hotmail.com>
2025-02-23 20:56:35 +0800

    check winner function

--- script.js
+++ script.js
@@ -20,13 +20,22 @@ function placeMarker(board, marker, position){
     } 
 }
 
-function displayWinner(){
-    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] !== ''){
-        console.log('Winner!');
-    } 
-
+function checkWinner(){
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8], //horizontal
+        [0,3,6], [1,4,7], [2,5,8], //vertical
+        [0,4,8], [2,4,6] //diagonal
+    ];
+    for (let i = 0; i < winningCombos.length; i++){
+        const [a,b,c] = winningCombos[i];
+        if(gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]){
+            console.log('Winner! is', gameBoard[a]);
+            return gameBoard[a];
+        }
+    }
+    return null;
 }
-placeMarker(gameBoard, 'X', 0);
-placeMarker(gameBoard, 'x', 1);
-placeMarker(gameBoard, 'X', 2);
-displayWinner();
+placeMarker(gameBoard, 'O', 0);
+placeMarker(gameBoard, 'O', 4);
+placeMarker(gameBoard, 'O', 8);
+console.log(checkWinner());
